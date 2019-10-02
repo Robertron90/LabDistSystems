a856299cd0bcea556c5ba54b33f0820c6465be57
Roberto Chavez <rchavez.intertec@gmail.com>
2019-10-02 22:24:41 +0300

    init

--- Client.py
+++ Client.py
@@ -1,15 +1,18 @@
 import socket, os, sys
 
-
-FILE = None
-HOST = '127.0.0.1'
-PORT = 8800
+FILE = sys.argv[1]
+HOST = sys.argv[2]
+PORT = sys.argv[3]
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    s.send(FILE.decode())
 
     with open(FILE, 'rb') as f:
       data = f.read(1024)
+      sended = 0
       while data:
         s.send(data)
+        sended += 1024
+        print(sended/os.path.getsize(FILE))
         data = f.read(1024)
--- Server.py
+++ Server.py
@@ -1,31 +1,30 @@
-import socket
+import socket, os
+from threading import Thread
 
 HOST = '0.0.0.0'
 PORT = 8800
 
 
-class FileThread(Thread);
+class FileThread(Thread):
+    def __init__(self, sock: socket.socket):
+        super().__init__(daemon=True)
+        self.sock = sock
 
-
-def __init__(self, sock: socket.socket)
-    super().__init__(daemon=True)
-    self.sock = sock
-
-
-def run(self);
-
-
-with self.sock:
-    with open("RECEIVEDFILE", 'wb') as f:
-        while True:
-            data = self.sock.recv(1024)
-            f.write(data)
-            if not data:
-                break
-return
+    def run(self):
+        with self.sock:
+            filename = self.sock.recv(7).decode()
+            if filename in os.listdir():
+                filename += "_copy"
+            with open(filename, 'wb') as f:
+                while True:
+                    data = self.sock.recv(1024)
+                    f.write(data)
+                    if not data:
+                        break
+        return
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(5)
     while True:
-        FileThread(s.accept[0]).start()
+        FileThread(s.accept()[0]).start()
